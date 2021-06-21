130ca8340b315cf4c53965a2ef008d041c585a1a
Julien Carmona <julien.carmona@insee.fr>
2021-06-21 09:58:32 +0200

    Maj Repository

--- src/main/java/com/maplr/testhockeygame/repository/PlayerRepository.java
+++ src/main/java/com/maplr/testhockeygame/repository/PlayerRepository.java
@@ -1,9 +1,15 @@
 package com.maplr.testhockeygame.repository;
 
 import com.maplr.testhockeygame.model.Player;
+import com.maplr.testhockeygame.model.Team;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface PlayerRepository extends CrudRepository <Player, Long> {
 
+    Player findByTeamAndIsCaptain(Team team, boolean b);
 
 }
--- src/main/java/com/maplr/testhockeygame/repository/TeamRepository.java
+++ src/main/java/com/maplr/testhockeygame/repository/TeamRepository.java
@@ -2,7 +2,9 @@ package com.maplr.testhockeygame.repository;
 
 import com.maplr.testhockeygame.model.Team;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeamRepository extends CrudRepository<Team, Long> {
 
     Team findByYear(long year);
