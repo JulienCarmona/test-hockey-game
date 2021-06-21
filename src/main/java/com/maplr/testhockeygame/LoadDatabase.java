5da627be2beb62b246b33ec8a20bcff852d6c496
Julien Carmona <julien.carmona@insee.fr>
2021-06-21 09:57:01 +0200

    Init programmaticaly

---
+++ src/main/java/com/maplr/testhockeygame/LoadDatabase.java
@@ -0,0 +1,37 @@
+package com.maplr.testhockeygame;
+
+import com.maplr.testhockeygame.model.Player;
+import com.maplr.testhockeygame.model.Team;
+import com.maplr.testhockeygame.repository.TeamRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class LoadDatabase {
+
+    @Bean
+    public CommandLineRunner teamInitializer(TeamRepository teamRepository) {
+        return args -> {
+            teamRepository.saveAll(Arrays.asList(
+                new Team("Dominique Ducharme", 2019, initListPlayers()),
+                new Team("Dominique Ducharme", 2020, initListPlayers())
+            ));
+        };
+    }
+
+    private List<Player> initListPlayers() {
+        return Arrays.asList(
+                new Player(31, "Carey", "Price", "goaltender", false),
+                new Player(14, "Nick", "Suzuki", "foward", false),
+                new Player(15, "Jesperi", "Kotkaniemi", "foward", false),
+                new Player(71, "Jake", "Evans", "foward", false),
+                new Player(27, "Alexander", "Romanov", "defenseman", false),
+                new Player(6, "Shea", "Weber", "defenseman", true)
+        );
+    }
+
+}
--- src/main/java/com/maplr/testhockeygame/TestHockeyGameApplication.java
+++ src/main/java/com/maplr/testhockeygame/TestHockeyGameApplication.java
@@ -17,31 +17,8 @@ import java.util.stream.StreamSupport;
 @SpringBootApplication
 public class TestHockeyGameApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(TestHockeyGameApplication.class, args);
 	}
 
-	@Bean
-	ApplicationRunner playerInitializer(PlayerRepository playerRepository) {
-		return args -> playerRepository.saveAll(Arrays.asList(
-				new Player(31, "Carey", "Price", "goaltender", false),
-				new Player(14, "Nick", "Suzuki", "foward", false),
-				new Player(15, "Jesperi", "Kotkaniemi", "foward", false),
-				new Player(71, "Jake", "Evans", "foward", false),
-				new Player(27, "Alexander", "Romanov", "defenseman", false),
-				new Player(6, "Shea", "Weber", "defenseman", true)
-		));
-	}
-
-	@Bean
-	public ApplicationRunner teamInitializer(TeamRepository teamRepository, PlayerRepository playerRepository) {
-		List<Player> players = StreamSupport
-				.stream(playerRepository.findAll().spliterator(), false)
-				.collect(Collectors.toList());
-		return args -> teamRepository.saveAll(Arrays.asList(
-				new Team("Dominique Ducharme", 2019, players),
-				new Team("Dominique Ducharme", 2020, players)
-		));
-	}
 }
